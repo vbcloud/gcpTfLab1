b7ca12be4cc21449bbafde95d4e3da36fe8a6b65
vbru0 <vitobrown718@gmail.com>
2025-05-09 01:12:49 -0400

    add .gitignore

---
+++ 0-authentication.tf
@@ -0,0 +1,6 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "thailand-433607"
+  region  = "us-central1"
+  credentials = "thailand-433607-6c00ddb70942.json"
+}
---
+++ 1-backend.tf
@@ -0,0 +1,21 @@
+# https://www.terraform.io/language/settings/backends/gcs
+terraform {
+  backend "gcs" {
+    bucket = "backendformytf"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+resource "google_compute_disk" "grafana_disk" {
+  name  = "grafana-disk"
+  type  = "pd-standard"
+  zone  = "us-central1-a"
+  size  = "10"
+}
